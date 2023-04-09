7110307c541581a7f4c05fd59f33f3aec03b42e1
Jailton Cruz <42894082+jailtoncruz@users.noreply.github.com>
2023-04-09 00:17:48 -0300

    chore: add images and environment variables

--- api/src/controllers/ItemsController.ts
+++ api/src/controllers/ItemsController.ts
@@ -1,21 +1,22 @@
 import { Request, Response } from 'express'
 import knex from '../database/connection';
+import { API_URL } from '../core/constants/environments';
 
 class ItemsController {
-    
-    async index(req: Request, res: Response) {
-        const items = await knex('items').select('*');
 
+    async index(req: Request, res: Response) {
+
+        const items = await knex('items').select('*');
         const serializedItems = items.map(item => {
             return {
                 id: item.id,
                 title: item.title,
-                image_url: `http://192.168.0.10:3333/uploads/${item.image}`,
-            }; 
+                image_url: `${API_URL}/uploads/${item.image}`,
+            };
         });
 
         return res.json(serializedItems);
-}
+    }
 
 }
 
--- api/src/core/constants/environments.ts
+++ api/src/core/constants/environments.ts
@@ -5,4 +5,6 @@ export const DB_HOST: string = process.env.DB_HOST ?? "localhost"
 export const DB_PORT: number = Number(process.env.DB_PORT ?? 5432);
 export const DB_USER: string = process.env.DB_USER ?? "ecoleta"
 export const DB_PASSWORD: string = String(process.env.DB_PASSWORD)
-export const DB_DATABASE: string = process.env.DB_DATABASE ?? "ecoleta"
+export const DB_DATABASE: string = process.env.DB_DATABASE ?? "ecoleta";
+
+export const API_URL = process.env.API_URL;
